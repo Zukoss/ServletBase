6ee8f5a05c452fbba7302e17001a10dd0a554059
Paul <pawelzukowski22@gmail.com>
2020-10-03 15:14:44 +0200

    T2-Entities. AppUser and Tweet models created.

--- src/main/java/Models/Tweet.java
+++ src/main/java/Models/Tweet.java
@@ -1,5 +1,58 @@
-package models;
+package Models;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tweet")
 public class Tweet {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "published_at")
+    @CreationTimestamp
+    private Date publishedAt;
+    private String message;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "author_fk")
+    private AppUser author;
+
+    public Tweet() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(Date publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public AppUser getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AppUser author) {
+        this.author = author;
+    }
 }
